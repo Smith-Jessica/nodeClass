29248501df70fe1ac3258ab40ccd911ffe5a4d97
Smith-Jessica <jpeters2153@gmail.com>
2019-11-16 17:06:46 -0700

    figured out how to get calculateRate working and pass in values from the form and how to render the results to the results page

--- index.js
+++ index.js
@@ -7,100 +7,65 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/getRate', (req, res) => res.render('pages/results'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/getRate', function (req, res) { var mtype = req.query.mtype; var weight = req.query.weight; calculateRate(mtype, weight, function (rate) { res.render('pages/results',{ rate : rate }); }) })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
-  function calculateRate(mtype, weight) {
-  switch(mtype) {
-    case '1':
-      //Letters (Stamped)
-      if(weight <= 1 && weight > 0) {
-        return 0.55;
-      }
-      else if(weight <= 2 && weight > 1) {
-        return 0.70;
-      }
-      else if(weight <= 3 && weight > 2) {
-        return 0.85;
-      }
-      else {
-        return 0;
-      }
-    case '2':
-       //Letters (Metered)
-       if(weight <= 1 && weight > 0) {
-        return 0.50;
-      }
-      else if(weight <= 2 && weight > 1) {
-        return 0.65;
-      }
-      else if(weight <= 3 && weight > 2) {
-        return 0.80;
-      }
-      else {
-        return 0;
-      }
-    case '3':
-       //Large Envelopes (Flats)
-       if(weight <= 1 && weight > 0) {
-        return 1.00;
-      }
-      else if(weight <= 2 && weight > 1) {
-        return 1.15;
-      }
-      else if(weight <= 3 && weight > 2) {
-        return 1.30;
-      }
-      else if(weight <= 4 && weight > 3) {
-        return 1.45;
-      }
-      else if(weight <= 5 && weight > 4) {
-        return 1.60;
-      }
-      else if(weight <= 6 && weight > 5) {
-        return 1.75;
-      }
-      else if(weight <= 7 && weight > 6) {
-        return 1.90;
-      }
-      else if(weight <= 8 && weight > 7) {
-        return 2.05;
-      }
-      else if(weight <= 9 && weight > 8) {
-        return 2.20;
-      }
-      else if(weight <= 10 && weight > 9) {
-        return 2.35;
-      }
-      else if(weight <= 11 && weight > 10) {
-        return 2.50;
-      }
-      else if(weight <= 12 && weight > 11) {
-        return 2.65;
-      }
-      else if(weight <= 13 && weight > 12) {
-        return 2.80;
-      }
-      else {
-        return 0;
-      }
-    case '4':
-          //First-Class Package Service—Retail
-          if(weight <= 4 && weight > 0) {
-           return 3.66;
-         }
-         else if(weight <= 8 && weight > 4) {
-           return 4.39;
-         }
-         else if(weight <= 12 && weight > 8) {
-           return 5.19;
-         }
-         else if(weight <= 13 && weight > 12) {
-           return 5.71;
-         }
-         else {
-           return 0;
-         }
-  }
-  }
+
+function calculateRate(mtype, weight, callback) {
+
+  //Letters (Stamped)
+  if (mtype == 1 && weight <= 1 && weight > 0)
+    callback(0.55);
+  else if (mtype == 1 && weight <= 2 && weight > 1)
+    callback(0.70);
+  else if (mtype == 1 && weight <= 3 && weight > 2)
+    callback(0.85);
+
+  //Letters (Metered)
+  if (mtype == 2 && weight <= 1 && weight > 0)
+    callback(0.50);
+  else if (mtype == 2 && weight <= 2 && weight > 1)
+    callback(0.65);
+  else if (mtype == 2 && weight <= 3 && weight > 2)
+    callback(0.80);
+
+  //Large Envelopes (Flats)
+  if (mtype == 3 && weight <= 1 && weight > 0)
+    callback(1.00);
+  else if (mtype == 3 && weight <= 2 && weight > 1)
+    callback(1.15);
+  else if (mtype == 3 && weight <= 3 && weight > 2)
+    callback(1.30);
+  else if (mtype == 3 && weight <= 4 && weight > 3)
+    callback(1.45);
+  else if (mtype == 3 && weight <= 5 && weight > 4)
+    callback(1.60);
+  else if (mtype == 3 && weight <= 6 && weight > 5)
+    callback(1.75);
+  else if (mtype == 3 && weight <= 7 && weight > 6)
+    callback(1.90);
+  else if (mtype == 3 && weight <= 8 && weight > 7)
+    callback(2.05);
+  else if (mtype == 3 && weight <= 9 && weight > 8)
+    callback(2.20);
+  else if (mtype == 3 && weight <= 10 && weight > 9)
+    callback(2.35);
+  else if (mtype == 3 && weight <= 11 && weight > 10)
+    callback(2.50);
+  else if (mtype == 3 && weight <= 12 && weight > 11)
+    callback(2.65);
+  else if (mtype == 3 && weight <= 13 && weight > 12)
+    callback(2.80);
+
+  //First-Class Package Service—Retail
+  if (mtype == 4 && weight <= 4 && weight > 0)
+    callback(3.66);
+  else if (mtype == 4 && weight <= 8 && weight > 4)
+    callback(4.39);
+  else if (mtype == 4 && weight <= 12 && weight > 8)
+    callback(5.19);
+  else if (mtype == 4 && weight <= 13 && weight > 12)
+    callback(5.71);
+
+}
